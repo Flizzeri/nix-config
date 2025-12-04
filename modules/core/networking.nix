4a562d78df1760d8feff0a8affe7dc7279a92572
flizzeri <filippo.lizzeri@gmail.com>
2025-12-04 23:11:49 +0100

    networking tweaks

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -3,6 +3,12 @@
 {
   networking.networkmanager.enable = true;
 
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;   # name resolution for .local over IPv4
+    openFirewall = true;
+  };
+
   # Basic firewall on, SSH will be selectively opened in the ssh module.
   networking.firewall.enable = true;
 }
