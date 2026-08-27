5228c8c813500fda2b72d1da97ecc25422b8796a
flizzeri <filippo.lizzeri@gmail.com>
2026-08-27 16:40:10 +0200

    fixed avahi

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -1,15 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  networking.networkmanager.enable = true;
-
   services.avahi = {
     enable = true;
-    nssmdns4 = true;   # name resolution for .local over IPv4
+    nssmdns4 = true;
     openFirewall = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
   };
 
   # Basic firewall on, SSH will be selectively opened in the ssh module.
   networking.firewall.enable = true;
 }
-
